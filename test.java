3aa5e9fcbd39b71e9258d33d887c8891b895b638
SAMIR-KARIYA <110136419+SAMIR-KARIYA@users.noreply.github.com>
2024-08-13 11:29:55 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 public class test{
     public static void main(String args[]){
         for(int i=0;i<5;i++)
-            System.out.println("TC5 Class");
+            System.out.println("TC5 Class updatde content");
     }
 }
